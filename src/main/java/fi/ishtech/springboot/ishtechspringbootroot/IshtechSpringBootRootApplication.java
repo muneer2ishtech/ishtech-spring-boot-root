6c7dca33695ccdc982e8c3416848044356a6b2c4
Muneer Ahmed Syed <muneer@ishtech.fi>
2024-03-03 14:05:18 +0200

    Initial code

---
+++ src/main/java/fi/ishtech/springboot/ishtechspringbootroot/IshtechSpringBootRootApplication.java
@@ -0,0 +1,13 @@
+package fi.ishtech.springboot.ishtechspringbootroot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IshtechSpringBootRootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IshtechSpringBootRootApplication.class, args);
+	}
+
+}
